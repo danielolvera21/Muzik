f135fe8934d5b9767d0278a57494cc34004c4a49
Man E <mannyportillo77@gmail.com>
2021-10-07 01:41:35 -0600

    added emojis

--- client/src/components/Donation/index.js
+++ client/src/components/Donation/index.js
@@ -4,7 +4,9 @@ import React from 'react';
 function Donation() {
     return (
         <section className = "my-5 about-border donate-box">
-            <h1 className = "about-head" id = "about"> Donations Welcome</h1>
+            <h1 className = "about-head" id = "about">
+                <span role="img" aria-label="money-bag">💰</span>Donations Welcome<span role="img" aria-label="money-bag">💰</span>
+            </h1>
         </section>
     );
 }
