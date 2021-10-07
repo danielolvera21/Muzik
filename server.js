0de2a91c24f75c46bf111c0b7d76822a3dce27aa
Blaine Curren <blaine.curren@gmail.com>
2021-10-06 23:32:45 -0500

    set up server.js with mongoose / mongodb

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Muzik", {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(`🌎 Connected on localhost:${PORT}`));
