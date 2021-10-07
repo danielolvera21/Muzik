7e6df8eaf63ade9105e6d5f5e705b2011f1dc7ff
Man E <mannyportillo77@gmail.com>
2021-10-07 01:32:26 -0600

    created donations and contact us containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Hero from './components/Hero'
+import Hero from './components/Hero';
+import Contact from './components/Contact';
+import Donation from './components/Donation';
 import './App.css';
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
       <Hero></Hero>
       <main>
       <About></About>
+      <Donation></Donation>
+      <Contact></Contact>
       </main>
+      
     </div>
   );
 }
---
+++ client/src/components/Donation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Donation() {
+    return (
+        <section className = "my-5 about-border donate-box">
+            <h1 className = "about-head" id = "about"> Donations Welcome</h1>
+        </section>
+    );
+}
+
+export default Donation;
