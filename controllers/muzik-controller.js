f80e92e65db149ee7d02a1d4f2e5e6f041b8864f
Blaine Curren <blaine.curren@gmail.com>
2021-10-06 23:42:48 -0500

    updated muzik-controller.js with getAllMuzik and getMuzikById

--- controllers/muzik-controller.js
+++ controllers/muzik-controller.js
@@ -1,5 +1,31 @@
 const { Muzik } = require("../models");
 
-const muzikController = {};
+const muzikController = {
+  getAllMuzik(req, res) {
+    Muzik.find({})
+      .then((dbMuzikData) => res.json(dbMuzikData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // get Muzik by id
+  getMuzikById({ params }, res) {
+    Muzik.findOne({ _id: params.id })
+      .then((dbMuzikData) => {
+        // if no muzik is found, send 404
+        if (!dbMuzikData) {
+          res.status(404).json({ message: "No muzik found with this ID." });
+          return;
+        }
+        res.json(dbMuzikData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+};
 
 module.exports = muzikController;
