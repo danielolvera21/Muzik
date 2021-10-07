6bf6374bb29994395c417289171bad97e139b546
Man E <mannyportillo77@gmail.com>
2021-10-07 00:09:20 -0600

    added some styling

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 function About() {
     return (
-        <section className = "my-5">
-            <h1 id = "about"> About Muzik</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+        <section className = "my-5 about-border">
+            <h1 className = "about-head" id = "about"> About Muzik</h1>
+            <p className="centered">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
           aliquam iste repudiandae totam, dolore, nesciunt provident iusto atque
           error corrupti laborum cum sint, accusantium odio voluptatum. Ad beatae
           voluptatum facilis!</p>
