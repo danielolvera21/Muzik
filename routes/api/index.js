3456bce28646a7050e60c493913c0a1b3f88343b
Blaine Curren <blaine.curren@gmail.com>
2021-10-07 19:48:22 -0500

    added routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const muzikRoutes = require("./muzik-routes");
+
+// add prefix of `/muzik` to routes created in `muzik-routes.js`
+router.use("/muzik", muzikRoutes);
+
+module.exports = router;
