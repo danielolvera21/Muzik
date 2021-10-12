6652b50cfcb452dbfaa33b8d9ea7d7879c6c6e1e
Man E <mannyportillo77@gmail.com>
2021-10-11 22:46:55 -0600

    footer and about compnents

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 function About() {
     return (
-        <section className="my-5 about-border">
+        <section className="about">
             <h1 className="about-head" id="about"> About Muzik</h1>
+            <div className = "aboutImage"></div>
             <p className="centered">Muzik allows fans and critics alike to
                 leave reviews on the live show experience of their favorite bands and aritists!
                 Find out if that new up and coming artist is worth checking out, or
