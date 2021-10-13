37a5000d994636872761ec9e066e1d0bd600458f
Daniel O <danielolvera2188@gmail.com>
2021-10-12 18:54:23 -0600

    pull

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,33 @@ import DonationFillerThree from './components/DonationFillerThree';
 import DonationFillerFour from './components/DonationFillerFour';
 import Filler from './components/Filler';
 import Fillertwo from './components/Fillertwo';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 import "./App.css";
 
 function App() {
+
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
 
   const [categories] = useState([
     { name: "home", description: "main page" },
@@ -24,6 +48,8 @@ function App() {
     { name: "donate", description: "donate to us" },
     { name: "search", description: "search an artist" },
     { name: "live", description: "Live shows" },
+    { name: "login", description: "logs you in" },
+    { name: "signup", description: "signs you up" }
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
@@ -51,9 +77,9 @@ function App() {
         </main>
       ) : currentCategory.name === "contact" ? (
         <main>
-        <Filler></Filler>
-        <Fillertwo></Fillertwo>
-        <Contact></Contact>
+          <Filler></Filler>
+          <Fillertwo></Fillertwo>
+          <Contact></Contact>
         </main>
       ) : currentCategory.name === "donate" ? (
         <main>
@@ -62,7 +88,7 @@ function App() {
           <DonationFillerTwo></DonationFillerTwo>
           <DonationFillerOne></DonationFillerOne>
           <DonationFillerThree></DonationFillerThree>
-        
+
         </main>
       ) : currentCategory.name === "search" ? (
         <SearchArtist></SearchArtist>
@@ -73,7 +99,6 @@ function App() {
       <footer>
         <Footer></Footer>
       </footer>
-
     </div>
   );
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+import Auth from "../../utils/auth";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props;
+
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  }
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
@@ -10,13 +17,25 @@ function Nav(props) {
 
   return (
     <header>
-      <nav className="loginStuff">
-      <h1 className="header-title"><a href="/">Muzik</a></h1>
-      <div className = "loginSpacing">
-      <a href="/">Log In</a>
-      <a href="/">Sign Up</a>
+      <div className="loginStuff">
+        <h1 className="header-title"><a href="/">Muzik</a></h1>
+        <nav className="loginSpacing">
+          <Router>
+            {Auth.loggedIn() ? (
+              <>
+                <a href="/" onClick={logout}>
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <Link to="/login">Log In</Link>
+                <Link to="/signup">Sign Up</Link>
+              </>
+            )}
+          </Router>
+        </nav>
       </div>
-      </nav>
       <nav>
         <ul className="flex-row">
           {categories.map((category) => (
