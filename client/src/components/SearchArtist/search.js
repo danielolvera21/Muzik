f74ab57aa1c50d78aad0f263a706e9935d0af4fc
Man E <mannyportillo77@gmail.com>
2021-10-13 22:44:26 -0600

    final css touch ups and live link removed from nav

--- client/src/components/SearchArtist/search.js
+++ client/src/components/SearchArtist/search.js
@@ -29,12 +29,14 @@ function Search(props) {
         className="searched-artist-bar"
         placeholder="Search for an Artist..."
       />
+      <div>
       <input
         onClick={callSearchFunction}
         type="submit"
-        className="artist-search-button"
+        className = "artist-search-button"
         value="SEARCH"
       />
+      </div>
     </form>
   );
 }
