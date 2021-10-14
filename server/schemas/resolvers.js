eb40ec53b452e5bc628634b9535359b2946bcb69
Blaine Curren <blaine.curren@gmail.com>
2021-10-14 13:54:50 -0500

    added initial review mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,58 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { User, Review } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        // get all users
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('friends');
-        },
-
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-                .select('-__v -password')
-                .populate('friends');
-        }
+  Query: {
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("reviews");
     },
 
-    Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("reviews");
+    },
 
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+    reviews: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Review.find(params).sort({ createdAt: -1 });
+    },
+    review: async (parent, { _id }) => {
+      return Review.findOne({ _id });
+    },
+  },
 
-            const correctPw = await user.isCorrectPassword(password);
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            const token = signToken(user);
-            return { token, user };
-        },
+      const correctPw = await user.isCorrectPassword(password);
 
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            return { token, user };
-        }
-    }
-}
+      const token = signToken(user);
+      return { token, user };
+    },
 
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,29 +1,41 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+// moved type User above type Query - 21.1.6 says to do so, ask group members
 const typeDefs = gql`
-type Query {
-    me: User
-    user(username: String): User
-    users: [User]
-}
-
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
+    reviews: [Review]
     friends: [User]
-}
+  }
 
-type Mutation {
+  type Review {
+      _id: ID
+      reviewText: String
+      createdAt: String
+      username: String
+    reviews(username: String): [Review]
+  }
+
+  type Query {
+    me: User
+    user(username: String): User
+    users: [User]
+    reviews(username: String): [Review]
+    review(_id: ID!): Review
+  }
+
+  type Mutation {
     login(email: String!, password: String!): User
     addUser(username: String!, email: String!, password: String!): User
-}
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 `;
 
 module.exports = typeDefs;
